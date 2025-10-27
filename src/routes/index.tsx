2c9fc8c3aa2591136c83d2139404050079610cd4
QuocDL <quoclcph18659@gmail.com>
2025-10-27 16:31:21 +0700

    404 page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import { PublicRoute } from "./PublicRoute";
 import { App } from "antd";
 import { AdminRoute } from "./AdminRoute";
+import NotFoundPage from "../pages/NotFoundPage";
 
-const route = [...PublicRoute, ...AdminRoute];
+const route = [
+  ...PublicRoute,
+  ...AdminRoute,
+  { path: "*", element: <NotFoundPage /> },
+];
 
 const routes = createBrowserRouter(route);
 

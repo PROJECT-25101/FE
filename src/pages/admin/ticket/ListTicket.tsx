88812a722a45523b94e025fd62704a66734454fa
QuocDL <quoclcph18659@gmail.com>
2025-12-15 16:02:22 +0700

    FIX LIST TICKET FILTER

--- src/pages/admin/ticket/ListTicket.tsx
+++ src/pages/admin/ticket/ListTicket.tsx
@@ -1,16 +1,18 @@
+import { QrcodeOutlined } from "@ant-design/icons";
 import { useQuery } from "@tanstack/react-query";
-import { Table } from "antd";
+import { Link } from "react-router";
 import { QUERY_KEY } from "../../../common/constants/queryKey";
 import { useTable } from "../../../common/hooks/useTable";
 import { getAllOrder } from "../../../common/services/order.service";
+import type { IOrder } from "../../../common/types/Order";
+import TableCustom from "../../../components/common/TableCustom";
 import { columnTicket } from "./components/ColumnTicket";
 import FilterTicket from "./components/FilterTicket";
-import { QrcodeOutlined } from "@ant-design/icons";
-import { Link } from "react-router";
 
 const ListTicket = () => {
-  const { query, getSorterProps } = useTable();
-  const { data } = useQuery({
+  const { query, getSorterProps, onFilter, onSelectPaginateChange } =
+    useTable<IOrder>();
+  const { data: response, isLoading } = useQuery({
     queryKey: [
       QUERY_KEY.ORDER.ROOT,
       ...Object.keys(query),
@@ -26,6 +28,7 @@ const ListTicket = () => {
         ...query,
       }),
   });
+  const { data = [], meta } = response || {};
   return (
     <div className="bg-white w-full min-h-[70dvh] rounded-md shadow-sm px-6 py-4">
       <div className="flex items-center justify-between mb-4">
@@ -36,10 +39,16 @@ const ListTicket = () => {
       </div>
       <FilterTicket />
       <div className="mt-4">
-        <Table
-          bordered
+        <TableCustom<IOrder>
+          showPagination={(meta?.total as number) > 10}
+          isLoading={isLoading}
           columns={columnTicket(getSorterProps)}
-          dataSource={data?.data}
+          dataSource={data || []}
+          onFilter={onFilter}
+          onSelectPaginateChange={onSelectPaginateChange}
+          pageSize={meta?.limit || 10}
+          totalDocs={meta?.total}
+          currentPage={meta?.page || 1}
         />
       </div>
     </div>
